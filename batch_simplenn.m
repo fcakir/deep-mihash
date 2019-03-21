b01a18c16f4de5dc59fa710ad378b8c7c3b9bb23
fcakir <fcakirs@gmail.com>
2019-03-21 12:09:43 -0400

    Refactoring II

--- batch_simplenn.m
+++
@@ -1,9 +0,0 @@
-function [images, labels] = batch_simplenn(imdb, batch)
-images = imdb.images.data(:, :, :, batch) ;
-if rand > 0.5, images=fliplr(images) ; end
-if isempty(imdb.images.labels)
-    labels = [];
-else
-    labels = permute(imdb.images.labels(:, batch), [3 4 2 1]);
-end
-end
--- get_model.m
+++
@@ -1,9 +0,0 @@
-function [net, opts] = get_model(opts)
-% -----------------------------------------------------------------------------
-% calls the appropiate function in +models for neural network model construction
-% -----------------------------------------------------------------------------
-t0 = tic;
-modelFunc = str2func(sprintf('models.%s', opts.modelType));
-[net, opts.imageSize, opts.normalize] = modelFunc(opts);
-myLogInfo('%s in %.2fs', opts.modelType, toc(t0));
-end
