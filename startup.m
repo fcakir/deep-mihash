fdb82e5cc46fc2aeb12b16ef5f97815c94ff1b0f
fcakir <fcakirs@gmail.com>
2019-04-08 18:39:10 -0400

    Update startup.m

--- startup.m
+++ startup.m
@@ -4,15 +4,5 @@ addpath('util', 'batch-generator', 'encoder', 'getter', 'mihash', 'tester', ...
 [r, name] = unix('hostname');
 name = name(1:end-1);
 myLogInfo('Hostname = %s', name);
-
-if ismember(name, {'ivcgpu3.bu.edu' 'hopper', 'ivcgpu4.bu.edu'})
-	myLogInfo('Using MatConvNet-GPU');
-	run ./matconvnet/matlab/vl_setupnn
-elseif ismember(name, {'ivcgpu1.bu.edu' 'ivcgpu2.bu.edu'})
-	myLogInfo('Using MatConvNet-CuDNNv5');
-	run ./matconvnet_cudnn5/matlab/vl_setupnn
-else
-	myLogInfo('Using MatConvNet-CPU');
-	run ./matconvnet_cpu/matlab/vl_setupnn
-end
+run ./matconvnet/matlab/vl_setupnn
 myLogInfo('MatConvNet ready');
